0acf1f56331828c378311c5e0ae7da2176eae81d
VincenzoLivigni <enzolivigni13@gmail.com>
2026-02-06 12:21:53 +0100

    snack4 done

---
+++ Snack_4/functions_snack4.js
@@ -0,0 +1,18 @@
+/*
+Snack 4 - ✔️
+Crea un generatore di funzioni creaTimer
+
+✔️ - Scrivi una funzione creaTimer che accetta un tempo(in ms) e restituisce 
+una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!".
+*/
+
+function creaTimer(timer) {
+    return function () {
+        setTimeout(() => {
+            console.log("Tempo scaduto!");
+        }, timer)
+    }
+}
+
+const timer3s = creaTimer(3000)
+timer3s()
